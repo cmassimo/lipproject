e4f8c71cf73d996f5fef41738d52e996b8e064a1
Carlo Maria Massimo <cmassim@gmail.com>
2013-11-22 15:13:31 +0100

    implementata funzione I() (prima parte)

--- assegnamento-lex.ml.ml
+++ assegnamento-lex.ml.ml
@@ -21,8 +21,8 @@ exception EmptyNumber;
    un identificatore, un operatore o una keyword *)
 fun IsAlphaChar(c: char) : bool =
   if (c >= (#"a") andalso c <=(#"z")) orelse (c>= (#"A") andalso c <= (#"Z")) 
-      then true 
-      else false;
+  then true 
+  else false;
 
 (* riconosce se c e' un carattere numerico o no   *)
 fun IsDigitChar(c)=
@@ -117,14 +117,14 @@ N(c::l, n, b)=
   if IsDigitChar(c) 
   then
     let
-val x= ord(c)-ord(#"0")
+      val x= ord(c)-ord(#"0")
     in 
-N(l,n*10+x,b)
-      end
+      N(l, n*10+x ,b)
+    end
 else
-if b=true 
-then (NM(~n),c::l)
-else (NM(n),c::l);
+  if b=true 
+  then (NM(~n),c::l)
+  else (NM(n),c::l);
 
 
 (* Stato SC per riconoscere le stringhe tra virgolette    *)
@@ -151,6 +151,35 @@ S(nil, result) =
 (*  FUNZIONE I DA FARE   *)
 
 fun I(nil)= raise UnexpectedEndOfString |
+I(x::xs) =
+    if IsSpace(x) then I(xs)
+    else if x = #"$" then [SYM(str(x))]
+    
+    else if x = #"~" then let 
+            val res = N(xs, 0, true)
+        in
+            #1res::I(#2res)
+        end
+    else if IsDigitChar(x) then let
+            val res = N(x::xs, 0, false)
+        in
+            #1res::I(#2res)
+        end
+    else if IsSeparator(x) then SYM(str(x))::I(xs)
+
+    else if IsAlphaChar(x) then let
+            val res = S(x::xs, [])
+        in
+            #1res::I(#2res)
+        end
+    else if x = #"\"" then let
+            val res = SC(x::xs, [])
+        in
+            #1res::I(#2res)
+        end
+    else if IsOp(x) then OP(str(x))::I(xs)
+
+    else raise NotValidChar(x);
 (*_ _ _ _ _ _ _ _ _ _ _ *)
 
 
@@ -165,9 +194,9 @@ fun lexi(s : char list) :  token list =
 
 (* esempi di programmi in Lisp Kit per provare lexi *)
 
-val C="letrec  FACT = lambda ( X ) if  eq ( X 0 ) then 1 else  X*FACT(  X- 1 )and 
-G = lambda ( H L ) if  eq ( L  nil ) then L else cons( H(car( L ) ) G ( H cdr ( L ) )) 
-in G ( FACT cons(1 cons(2 cons(3 nil))) ) end $";
+(* val C="letrec  FACT = lambda ( X ) if  eq ( X 0 ) then 1 else  X*FACT(  X- 1 ) and G = lambda ( H L ) if  eq ( L  nil ) then L else cons( H(car( L ) ) G ( H cdr ( L ) )) in G ( FACT cons(1 cons(2 cons(3 nil))) ) end $"; *)
+
+val C="letrec  FACT = lambda ( X ) if  eq ( X 0 ) then 1 else  X*FACT(  X- 1 )and G = lambda ( H L ) if  eq ( L  nil ) then L else cons( H(car( L ) ) G ( H cdr ( L ) )) in G ( FACT cons(1 cons(2 cons(3 nil))) ) end $";
 
 val D= "let x=cons(\"ab\" cons(\"cd\" nil)) in if true then cons(\"01\" x) else nil end $";
 
