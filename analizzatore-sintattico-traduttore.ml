c5dd5ddb1664620612f0a2337426e025618dbbc5
Carlo Maria Massimo <cmassim@gmail.com>
2013-12-17 19:01:14 +0100

    quasi finito traduttore

--- analizzatore-sintattico-traduttore.ml
+++ analizzatore-sintattico-traduttore.ml
@@ -1,6 +1,6 @@
 (* PARSER LispKit  incompleto Novembre 2013*)
 
-use "assegnamento-lex.ml.ml";
+use "analizzatore-lessicale.ml";
 
 (* richiamare l'analizzatore lessicale *)
 
@@ -14,305 +14,297 @@ exception ex of string;
 
 (* funzioni per DEBUG ***************************)
 
-fun f(x:token):string=
-case x of 
-KEYWORD(a) => a |
-ID(a) => a |
-OP(a)=> a |
-SYM(a)=> a |
-STR(a)=> a |
-NM(a) => "NM" |
-BOOL(a)=> "BOOL" |
-Nil => "Nil";
+fun f(x : token) : string=
+  case x of 
+    KEYWORD(a) => a |
+    ID(a) => a |
+    OP(a)=> a |
+    SYM(a)=> a |
+    STR(a)=> a |
+    NM(a) => "NM" |
+    BOOL(a)=> "BOOL" |
+    Nil => "Nil";
 (*_=> "oggetto non previsto";*)
 
 fun g(nil)= "" |
-g(h::t)=  f(h) ^ g(t);
+    g(h::t)= f(h) ^ g(t);
 
 
 (*  AUSILIARIA *****************************)
-fun trova_stringa(nil,x)=
-  raise ex("la stringa termina prematuramente") |
-  trova_stringa(KEYWORD(a)::z,x)= a=x |
-  trova_stringa(OP(a)::z,x)= a=x |
-  trova_stringa(ID(a)::z,x)= a=x |
-  trova_stringa (SYM(a)::z,x)=a=x |
-  trova_stringa(STR(a)::z,x)= a=x|
-  trova_stringa(_,_)= false;
+fun trova_stringa(nil, x)= raise ex("la stringa termina prematuramente") |
+    trova_stringa(KEYWORD(a)::z, x)= a = x |
+    trova_stringa(OP(a)::z, x)= a = x |
+    trova_stringa(ID(a)::z, x)= a = x |
+    trova_stringa (SYM(a)::z, x)= a = x |
+    trova_stringa(STR(a)::z, x)= a = x |
+    trova_stringa(_, _)= false;
+
+
+datatype LKC=
+  VAR of string |
+  NUM of int |
+  BOO of B |
+  STRI of string |
+  NIL |
+  ADD of LKC*LKC |
+  SUB of LKC*LKC |
+  MULT of LKC*LKC |
+  DIV of LKC*LKC |
+  EQ of LKC*LKC |
+  LEQ of LKC*LKC |
+  CAR of LKC |
+  CDR of LKC |
+  CONS of LKC*LKC |
+  ATOM of LKC |
+  IF of LKC*LKC*LKC |
+  LAMBDA of LKC list * LKC |
+  CALL of LKC * LKC list |
+  LET of LKC * (LKC*LKC) list |
+  LETREC of LKC * (LKC*LKC) list;
 
 (* Analizzatore sintattico ****************************)
-val PROG= let (* usare la let forma una specie di namespace evitando involontari conflitti di nome tra le funzioni definite
+val PROG =
+  let (* usare la let forma una specie di namespace evitando involontari conflitti di nome tra le funzioni definite
 nelle diverse parti del programma *)
 
-  fun
-  Prog (nil): token list =
-    raise ex("Prog: terminazione imprevista")
-  |
-
-  Prog(KEYWORD(a)::c)=
-    if not(a="let" orelse a="letrec")
+    fun
+    Prog(nil)= raise ex("Prog: terminazione imprevista") |
+    Prog(KEYWORD(a)::c)=
+      if not(a = "let" orelse a = "letrec")
       then 
         raise ex("programma inizia senza let/letrec")
       else
 
         let 
-          val x1=Bind(c) 
+          val (trad_bind, bind) = Bind(c)
         in
-          if not(trova_stringa(x1,"in")) 
-            then raise Etwo("Prog: no in dopo bind",g(x1))
-            else
-
-              let 
-                val x2=Exp(tl(x1)) 
-              in
-                if not(trova_stringa(x2,"end"))
-                  then 
-      	           raise Etwo("no end alla fine del programma", g(x2) )
-                  else 
-      	           tl(x2)
-              end
-
-        end
-  |
-
-  Prog(_)= raise ex("programma inizia senza keyword")
-    
-  and
-
-  Exp(nil)=
-    raise ex("Exp: input termina prematuramente")
-  |
-  Exp(KEYWORD("let")::c)= Prog(KEYWORD("let")::c)
-  |
-  Exp(KEYWORD("letrec")::c)= Prog(KEYWORD("letrec")::c)
-  |
-  Exp(KEYWORD("lambda")::c)= Exp(esp_LAMB(c))
-  |
-  Exp(OP("cons")::c) = due_exp(c)
-  |     
-  Exp(OP("car")::c) = Exp(c)
-  |     
-  Exp(OP("cdr")::c) = Exp(c) 
-  |     
-  Exp(OP("eq")::c) = due_exp(c)
-  |     
-  Exp(OP("leq")::c) = due_exp(c)
-  |     
-  Exp(OP("atom")::c) = Exp(c)
-  |     
-  Exp(KEYWORD("if")::c) = 
-    let 
-      val x1=Exp(c)
-    in
-      if not (trova_stringa(x1,"then"))
-      then
-        raise Etwo("dopo if exp niente then",g(x1))
-      else
-        let 
-          val x2=Exp(tl(x1))
-        in
-          if  not(trova_stringa(x2,"else"))
-          then 
-            raise Etwo("dopo if_then niente else",g(x2))
+          if not(trova_stringa(bind, "in")) 
+          then raise Etwo("Prog: no in dopo bind", g(bind))
           else
-            Exp(tl(x2))
-        end
-    end
-  | (* espressioni aritmetiche *)
-    
-  Exp(x)= ExpA(x) (*5 ExpA::= T E1 *)
-    
-  and
 
-  ExpA(nil)=
-    raise ex("ExpA: input termina prematuramente")
-  |
-  ExpA(x) = E1(T(x))
+            let 
+              val (trad_exp, exp) = Exp(tl(bind))
+            in
+              if not(trova_stringa(exp, "end"))
+              then 
+                raise Etwo("no end alla fine del programma", g(exp))
+              else if (a = "let")
+                then (LET(trad_exp, trad_bind), tl(exp))
+                else (LETREC(trad_exp, trad_bind), tl(exp))
+            end
 
-  and
+        end |
+    Prog(_)= raise ex("programma inizia senza keyword")
+      
+    and
 
-  E1(nil)=
-    raise ex("E1: input termina prematuramente")
-  |
-  E1(OP("+")::c) = E1(T(c))
-  |
-  E1(OP("-")::c) = E1(T(c))
-  |
-  E1(s) = s
-
-  and
-
-  T(nil)=
-    raise ex("T: input termina prematuramente")
-  |
-  T(x) = T1(F(x))
-
-  and
-
-  T1(nil)=
-    raise ex("T1: input termina prematuramente")
-  |
-  T1(OP("*")::c) = T1(F(c))
-  |
-  T1(OP("/")::c) = T1(F(c))
-  |
-  T1(s) = s
-
-  and 
-
-  Exp_const(nil):token list= 
-    raise ex("Exp_const: input termina prematuramente")
-  |
-  Exp_const(NM(a)::c:token list)= c
-  |
-  Exp_const(Nil::c)=c
-  |
-  Exp_const(BOOL(a)::c)= c
-  |
-  Exp_const(STR(a)::c)= c
-  |
-  Exp_const(a)= raise NOCONST
-
-  and
-
-  F(nil)=
-    raise ex("F: input termina prematuramente")
-  |
-  F(x)= Exp_const(x) handle NOCONST =>
-    if (trova_stringa(x, "("))
-    then
+    Exp(nil)= raise ex("Exp: input termina prematuramente") |
+    Exp(KEYWORD("let")::c)= Prog(KEYWORD("let")::c) |
+    Exp(KEYWORD("letrec")::c)= Prog(KEYWORD("letrec")::c) |
+    Exp(KEYWORD("lambda")::c)=
       let
-        val ea = ExpA(x)
+        val (trad_lambda, lmbd) = esp_LAMB(c)
+        val (trad_exp, exp) = Exp(lmbd)
       in
-        if not(trova_stringa(ea, ")"))
-        then raise Etwo("manca ) dopo (ExpA", g(ea))
-        else tl(ea)
-      end
-    else Y(var(x))
-  (* espressione non costante *)
-
-  and
-
-  Y(nil)=
-    raise ex("Y: input termina prematuramente")
-  |
-  Y(SYM("(")::c)=
-    let
-      val se = Seq_Exp(c)
-    in
-      if not(trova_stringa(se, ")"))
-      then raise Etwo("Y: manca ) dopo Seq_Exp", g(se))
-      else tl(se)
-    end
-  |
-  Y(s) = s
-
-  and
-
-  Seq_Exp(nil)=
-    raise ex("Seq_Exp: input termina prematuramente")
-  |
-  Seq_Exp(s) =
-    if not(trova_stringa(s, ")")) (* non sono nella produzione epsilon *)
-      then Z(Exp(s))
-      else s
-
-  and
-
-  Z(nil)= 
-    raise ex("Z: input termina prematuramente")
-  |
-  Z(SYM(",")::c)= Seq_Exp(c)
-  |
-  Z(s)= s
-
-  and
-
-  Seq_Var(nil):token list=
-    raise ex("Seq_Var: la stringa termina prematuramente")
-  |
-  Seq_Var(x)=
-    if not(trova_stringa(x,")"))
-      then
-        Seq_Var(var(x))
-      else
-        x (* non consumo la ) che va consumata in Exp *)
-
-  and
-
-  var(nil):token list =
-    raise ex("var: la stringa termina prematuramente")
-  |
-  var(ID(a)::c)= c 
-  |
-  var(x)= raise Etwo("oggetto estraneo in Var_List", g(x))
-
-  (*2 Bind::= var = Exp X 3 X::= and Bind | epsilon *)
-
-  and
-
-  Bind(nil)=
-    raise ex("Z: input termina prematuramente")
-  |
-  Bind(s)=
-    let
-      val va = var(s)
-    in
-      if not(trova_stringa(va, "="))
-        then raise Etwo("Bind: manca = dopo var in Bind", g(va))
-        else X(Exp(tl(va)))
-    end
-
-  and
-
-  X(nil)=
-    raise ex("X: input termina prematuramente")
-  |
-  X(KEYWORD("and")::c)= Bind(c)
-  |
-  X(s)= s
-
-  and
-
-  esp_LAMB(c)= (* serve per riconoscere la sequenza di parametri formali di una lambda *)
-    if not (trova_stringa(c,"("))
-    then 
-      raise ex("esp_LAMB: manca (")
-    else 
+        (LAMBDA(trad_lambda, trad_exp), exp)
+      end |
+    Exp(OP("cons")::c)= let val (trad_cons, cns) = due_exp(c) in (CONS(trad_cons), cns) end |
+    Exp(OP("car")::c)= let val (trad_exp, exp) = Exp(c) in (CAR(trad_exp), exp) end |
+    Exp(OP("cdr")::c)= let val (trad_exp, exp) = Exp(c) in (CDR(trad_exp), exp) end |
+    Exp(OP("eq")::c)= let val (trad_exp, exp) = due_exp(c) in (EQ(trad_exp), exp) end |
+    Exp(OP("leq")::c)= let val (trad_exp, exp) = due_exp(c) in (LEQ(trad_exp), exp) end |
+    Exp(OP("atom")::c)= let val (trad_exp, exp) = Exp(c) in (ATOM(trad_exp), exp) end |
+    Exp(KEYWORD("if")::c)= 
       let 
-        val c1=Seq_Var(tl(c)) 
+        val (trad_exp, exp)= Exp(c)
       in
-        if trova_stringa(c1,")")
-          then 
-  	       tl(c1)
-          else
-            raise ex("non si chiude la )")
-      end
-
-  and
-
-  due_exp(c):token list=
-    if not (trova_stringa(c,"("))
-    then 
-      raise ex("la stringa termina prematuramente")
-    else 
-      let 
-        val c1 =Exp(tl(c)) 
-      in
-        if trova_stringa(c1,",")
+        if not (trova_stringa(exp, "then"))
         then
-          let
-            val c2= Exp(tl(c1))
-          in
-            if trova_stringa(c2,")")
-            then 
-              tl(c2)
-            else
-              raise ex("non si chiude la )")
-          end
+          raise Etwo("dopo if exp niente then", g(exp))
         else
-          raise Etwo("non c'e' , tra 2 expr",g(c1)) 
+          let 
+            val (trad_then, thn) = Exp(tl(exp))
+          in
+            if not(trova_stringa(thn, "else"))
+            then 
+              raise Etwo("dopo if_then niente else", g(thn))
+            else
+              let val (trad_else, els) = Exp(tl(thn)) in (IF(trad_exp, trad_then, trad_else), els) end
+          end
+      end | (* espressioni aritmetiche *)
+    Exp(tlist)= ExpA(tlist) (*5 ExpA::= T E1 *)
+      
+    and
+
+    ExpA(nil)= raise ex("ExpA: input termina prematuramente") |
+    ExpA(tlist)= let val (trad_t, texp) = T(tlist) in E1(trad_t, texp) end
+
+    and
+
+    E1(PO : LKC, nil)= raise ex("E1: input termina prematuramente") |
+    E1(PO : LKC, OP("+")::c)= let val (trad_t, texp) = T(c) in E1(ADD(PO, trad_t), texp) end |
+    E1(PO : LKC, OP("-")::c)= let val (trad_t, texp) = T(c) in E1(SUB(PO, trad_t), texp) end |
+    E1(PO : LKC, tlist) = (PO, tlist)
+
+    and
+
+    T(nil)= raise ex("T: input termina prematuramente") |
+    T(tlist)= let val (trad_f, fexp) = F(tlist) in T1(trad_f, fexp) end
+
+    and
+
+    T1(PO : LKC, nil)= raise ex("T1: input termina prematuramente") |
+    T1(PO : LKC, OP("*")::c)= let val (trad_f, fexp) = F(c) in T1(MULT(PO, trad_f), fexp) end |
+    T1(PO : LKC, OP("/")::c)= let val (trad_f, fexp) = F(c) in T1(DIV(PO, trad_f), fexp) end |
+    T1(PO : LKC, tlist) = (PO, tlist)
+
+    and 
+
+    Exp_const(nil)= raise ex("Exp_const: input termina prematuramente") |
+    Exp_const(NM(a)::c : token list)= (NUM(a), c) |
+    Exp_const(Nil::c)= (NIL, c) |
+    Exp_const(BOOL(a)::c)= (BOO(a), c) |
+    Exp_const(STR(a)::c)= (STRI(a), c) |
+    Exp_const(tlist)= raise NOCONST
+
+    and
+
+    F(nil)= raise ex("F: input termina prematuramente") |
+    F(tlist)= Exp_const(tlist) handle NOCONST =>
+      if (trova_stringa(tlist, "("))
+      then
+        let
+          val (trad_expa, expa) = ExpA(tlist)
+        in
+          if not(trova_stringa(expa, ")"))
+          then raise Etwo("manca ) dopo (ExpA", g(expa))
+          else (trad_expa, tl(expa))
+        end
+      else
+        let
+          val (trad_var, var_exp) = var(tlist)
+          val (trad_y, yexp) = Y(var_exp)
+        in
+          (CALL(trad_var, trad_y), yexp)
+        end
+    (* espressione non costante *)
+
+    and
+
+    Y(nil)= raise ex("Y: input termina prematuramente") |
+    Y(SYM("(")::c)=
+      let
+        val (trad_seqexp, seqexp) = Seq_Exp(c)
+      in
+        if not(trova_stringa(seqexp, ")"))
+        then raise Etwo("Y: manca ) dopo Seq_Exp", g(seqexp))
+        else (trad_seqexp, tl(seqexp))
+      end |
+    Y(tlist) = (nil, tlist)
+
+    and
+
+    Seq_Exp(nil)= raise ex("Seq_Exp: input termina prematuramente") |
+    Seq_Exp(tlist)=
+      if not(trova_stringa(tlist, ")")) (* non sono nella produzione epsilon *)
+      then
+        let
+          val (trad_exp, exp) = Exp(tlist)
+          val (trad_z, zexp) = Z(exp)
+        in
+          (trad_exp::trad_z, zexp)
+        end
+      else (nil, tlist)
+
+    and
+
+    Z(nil)= raise ex("Z: input termina prematuramente") |
+    Z(SYM(",")::c)= let val (trad_seqexp, seqexp) = Seq_Exp(c) in (trad_seqexp, seqexp) end |
+    Z(tlist)= (nil, tlist)
+
+    and
+
+    Seq_Var(nil)= raise ex("Seq_Var: la stringa termina prematuramente") |
+    Seq_Var(tlist)=
+      if not(trova_stringa(tlist, ")"))
+      then
+        let
+          val (trad_var, var_exp) = var(tlist)
+          val (trad_seqvar, seqvar) = Seq_Var(var_exp)
+        in
+          (trad_var::trad_seqvar, seqvar)
+        end
+      else (nil, tlist) (* non consumo la ) che va consumata in Exp *)
+
+    and
+
+    var(nil)= raise ex("var: la stringa termina prematuramente") |
+    var(ID(a)::c)= (VAR(a), c) |
+    var(tlist)= raise Etwo("oggetto estraneo in Var_List", g(tlist))
+
+    (*2 Bind::= var = Exp X 3 X::= and Bind | epsilon *)
+
+    and
+
+    Bind(nil)= raise ex("Z: input termina prematuramente") |
+    Bind(tlist)=
+      let
+        val (trad_var, var_exp) = var(tlist)
+      in
+        if not(trova_stringa(var_exp, "="))
+        then raise Etwo("Bind: manca = dopo var in Bind", g(var_exp))
+        else
+          let
+            val (trad_exp, exp) = Exp(tl(var_exp))
+            val (trad_x, xexp) = X(exp)
+          in
+            ((trad_var, trad_exp)::trad_x, xexp)
+          end
       end
+
+    and
+
+    X(nil)= raise ex("X: input termina prematuramente") |
+    X(KEYWORD("and")::c)= Bind(c) |
+    X(tlist)= (nil, tlist)
+
+    and
+
+    esp_LAMB(c)= (* serve per riconoscere la sequenza di parametri formali di una lambda *)
+      if not(trova_stringa(c, "("))
+      then raise ex("esp_LAMB: manca (")
+      else 
+        let 
+          val (trad_seqvar, seqvar) = Seq_Var(tl(c))
+        in
+          if trova_stringa(seqvar,")")
+          then (nil, tl(seqvar))
+          else raise ex("non si chiude la )")
+        end
+
+    and
+
+    due_exp(c)=
+      if not (trova_stringa(c, "("))
+      then raise ex("la stringa termina prematuramente")
+      else 
+        let 
+          val (trad_exp, exp) = Exp(tl(c)) 
+        in
+          if trova_stringa(exp, ",")
+          then
+
+            let
+              val (trad_exp2, exp2) = Exp(tl(exp))
+            in
+              if trova_stringa(exp2, ")")
+              then ((trad_exp, trad_exp2), tl(exp2))
+              else raise ex("non si chiude la )")
+            end
+
+          else raise Etwo("non c'e' , tra 2 expr",g(exp)) 
+        end
+
 
   in
     Prog
@@ -322,13 +314,13 @@ nelle diverse parti del programma *)
   (*esempi per test*)
 
 
-val S="letrec  FACT = lambda ( X ) if  eq ( X, 0 ) then 1 else  X*FACT(  X - 1 )"^
+(*val S="letrec  FACT = lambda ( X ) if  eq ( X, 0 ) then 1 else  X*FACT(  X - 1 )"^
 "and G = lambda ( H L ) if  eq ( nil, L ) then L else cons( H(car( L ) ),"^ 
 "G ( H, cdr ( L ) )) in G ( FACT, cons(1 ,cons(2, cons(3, nil))) ) end $";
+*)
 
 
-
-(*val S= "let x= 5 and y= 6 in x * 3 + y * 2 * x + x * y end $";*)
+val S= "let x= 5 and y= 6 in x * 3 + y * 2 * x + x * y end $";
 
 
 
@@ -340,16 +332,17 @@ val S="letrec  FACT = lambda ( X ) if  eq ( X, 0 ) then 1 else  X*FACT(  X - 1 )
 
 
 
-val D= lexi(explode(S));
+val D = lexi(explode(S));
 
-let val q=(PROG(D) 
-  handle Etwo(x,y)=> let val a=print("ERRORE "^x) and b=print("\n") and c=print(y) in nil end
-  handle ex(s) => let val a =print(s) in nil end
-  handle NOCONST => let val a=print("NOCONST") in nil end)
+val asd = let
+  val q = (PROG(D) 
+    handle Etwo(x,y)=> let val a = print("ERRORE "^x) and b = print("\n") and c = print(y) in (NIL, nil) end
+    handle ex(s) => let val a = print(s) in (NIL, nil) end
+    handle NOCONST => let val a = print("NOCONST") in (NIL, nil) end)
 in 
   case q of
-    nil => "Bad 1" |
-    (SYM(a)::t) => a |
-    _ => "Bad 2"
+    (NIL, nil) => NIL |
+    (x, SYM(a)::t) => x |
+    (_, _) => NIL
 end;
 
